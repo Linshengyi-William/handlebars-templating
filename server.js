7e264f6c7963eae69b18e16a183961121a2bd25a
Rob Hess <robwhess@gmail.com>
2020-11-24 13:55:54 -0800

    Add code from 2nd "lecture" (server-side templating).

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var path = require('path');
 var express = require('express');
-var app = express();
+var exphbs = require('express-handlebars');
 
+var app = express();
 var port = 8000;
+
+app.engine('handlebars', exphbs({ defaultLayout: null }));
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
@@ -17,6 +21,34 @@ var availablePeople = [
   'finn',
   'r2d2'
 ];
+
+app.get('/test', function (req, res, next) {
+  res.status(200).render('photoPage', {
+    name: "Testing Person",
+    photos: [
+      {
+        url: "http://placekitten.com/480/480?image=3",
+        caption: "Kitty from the server side #1."
+      },
+      {
+        url: "http://placekitten.com/480/480?image=4",
+        caption: "Kitty from the server side #2."
+      },
+      {
+        url: "http://placekitten.com/480/480?image=5",
+        caption: "Kitty from the server side #3."
+      },
+      {
+        url: "http://placekitten.com/480/480?image=6",
+        caption: "Kitty from the server side #4."
+      },
+      {
+        url: "http://placekitten.com/480/480?image=7",
+        caption: "Kitty from the server side #5."
+      }
+    ]
+  });
+});
 
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
